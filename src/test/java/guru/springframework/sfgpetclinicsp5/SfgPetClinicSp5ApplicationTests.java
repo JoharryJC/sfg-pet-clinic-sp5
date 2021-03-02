47a71663d4ef613c0576bcd9fffa7e547f96a886
JoharryJC <41237769+JoharryJC@users.noreply.github.com>
2021-03-02 22:22:23 +0100

    Initial Project Add

---
+++ src/test/java/guru/springframework/sfgpetclinicsp5/SfgPetClinicSp5ApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinicsp5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicSp5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
