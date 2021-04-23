8a375b4f67c2eb7dc4c2a51209ce7a83a7c848e5
JoharryJC <41237769+JoharryJC@users.noreply.github.com>
2021-04-23 22:35:08 +0200

    updated owner with JPA entity mappings. Close #37

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinicsp5/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinicsp5/model/Person.java
@@ -1,15 +1,23 @@
 package guru.springframework.sfgpetclinicsp5.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
 /**
  * 
  */
-public class Person extends BaseEntity {
 
+@MappedSuperclass
+public class Person extends BaseEntity {
 	
 	private static final long serialVersionUID = -6458205070801349459L;
 	
+	@Column(name = "first_name")
 	private String firstName;
+	
+	@Column(name = "last_name")
 	private String lastName;
+	
 	public String getFirstName() {
 		return firstName;
 	}
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinicsp5/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinicsp5/model/PetType.java
@@ -1,7 +1,14 @@
 package guru.springframework.sfgpetclinicsp5.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity {
 
+	@Column(name = "name")
 	private String name;
 
 	public String getName() {
@@ -11,6 +18,5 @@ public class PetType extends BaseEntity {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	
+		
 }
